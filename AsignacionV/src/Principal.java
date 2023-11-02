8f156461aa84a610489a438fd558422692dd84de
Sofia Cano Aguirre <canoaguirresofia@gmail.com>
2023-11-02 09:12:59 -0500

    Primer

---
+++ AsignacionV/src/Principal.java
@@ -0,0 +1,8 @@
+import gui.VentanaPrincipal;
+
+public class Principal {
+	public static void main(String arg[]) {
+		VentanaPrincipal miVentana = new VentanaPrincipal();
+		miVentana.setVisible(true);
+	}
+}
